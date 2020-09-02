9f4871580795b641aacda6b0c3ad3770ab4398a0
Steven Ijames <ijamesproductions1@gmail.com>
2020-09-02 01:20:54 -0600

    findall method working

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -1,34 +1,34 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the FoodssController
 module.exports = {
-  findAll: function(req, res) {
-    db.Book
+  findAll: function (req, res) {
+    db.Foods
       .find(req.query)
-      .sort({ date: -1 })
+      .sort({ product: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  findById: function(req, res) {
-    db.Book
+  findById: function (req, res) {
+    db.Foods
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
-    db.Book
+  create: function (req, res) {
+    db.Foods
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  update: function(req, res) {
-    db.Book
+  update: function (req, res) {
+    db.Foods
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  remove: function(req, res) {
-    db.Book
+  remove: function (req, res) {
+    db.Foods
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
