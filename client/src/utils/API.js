98ed51cd654e918fd9c72444fa244e7ab1af8678
Lady Stock <nikki.castlea@gmail.com>
2020-09-04 15:02:38 -0600

    added s to http api call request

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
     getMarkets: searchTerm => {
-        return axios.get(`http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=${searchTerm}`)
+        return axios.get(`https://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=${searchTerm}`)
     },
 
     getFood: searchTerm => {
